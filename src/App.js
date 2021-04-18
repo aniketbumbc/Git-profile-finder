206536d57fce2fcddb2f321379bbbc915ea55d77
aniket <aniket.umbc@gmail.com>
2021-04-18 18:58:55 +0530

    Application Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './componets/layout/Navbar';
-import Users from './componets/users/Users';
-import Search from './componets/users/Search';
 import Alert from './componets/layout/Alert';
 import About from './componets/pages/About';
 import User from './componets/users/User';
+import Home from './componets/pages/Home';
+import NotFound from './componets/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -20,18 +20,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
